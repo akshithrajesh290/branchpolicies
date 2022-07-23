9b2075604694ff16e93f0e5b89cfa115955b60ae
akshithrajesh <akshithrajesh290@gmail.com>
2022-07-23 15:51:07 +0100

    ALter Scripts added

---
+++ Deployement Scripts/AlterScripts.sql
@@ -0,0 +1,13 @@
+CREATE TABLE [dbo].[Student123]
+(
+   EmpId integer, 
+    FirstName varchar(20), 
+    LastName varchar(20), 
+    Email varchar(25), 
+    PhoneNo varchar(25), 
+    Salary integer
+);
+
+
+ALTER TABLE [dbo].[Student]
+ADD Email varchar(255);
